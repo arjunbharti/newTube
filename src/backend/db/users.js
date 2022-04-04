d70bdce75b9f016356c2566e1d0c9b9421560ea2
arjunbharti <arjunbharti774@gmail.com>
2022-04-04 16:19:04 +0530

    feat: created login functionality

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -1,18 +1,13 @@
 import { v4 as uuid } from "uuid";
 import { formatDate } from "../utils/authUtils";
-/**
- * User Database can be added here.
- * You can add default users of your wish with different attributes
- * Every user will have likes (Likes are set to 0 by default), History Array, Playlists Array (added Watch Later Playlist in it by default) by default
- * */
 
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshBalika123",
+    firstName: "Arjun",
+    lastName: "Bharti",
+    email: "arjun123@test.com",
+    password: "arjun123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
